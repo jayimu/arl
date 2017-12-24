5b7d743f37bc64fcd17041c192cfede2d3d5909f
lining <helloworld@126.com>
2017-12-24 10:42:30 +0800

    udp

--- src/chapter15/demo15.07.py
+++ src/chapter15/demo15.07.py
@@ -1,16 +1,14 @@
-import select
-import socket
-
-HOST = '127.0.0.1'
-PORT = 3000
-timeout =500
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# 设置连接超时
-s.settimeout(timeout)
-s.connect((HOST, PORT))
-s.sendall("aaa".encode(encoding='utf_8'))
-# 恢复默认超时设置
-#s.settimeout(None)
-#s.connect((HOST, PORT))
-#s.sendall('msg')
+from socket import *
+from time import ctime
+host = ''
+port = 9876
+bufferSize = 1024
+addr = (host, port)
+udpServerSocket = socket(AF_INET, SOCK_DGRAM)
+udpServerSocket.bind(addr)
+while True:
+    print('正在等待消息......')
+    data, addr = udpServerSocket.recvfrom(bufferSize)
+    udpServerSocket.sendto(ctime().encode(encoding='utf-8') + b' ' + data,addr)
+    print('客户端地址：',addr)
+udpServerSocket.close()
