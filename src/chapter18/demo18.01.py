b4a5d53da74e939eccd0ab82609a59293cdad0d9
lining <282662996@qq.com>
2018-01-05 18:05:08 +0800

    18章开始

---
+++ src/chapter18/demo18.01.py
@@ -0,0 +1,17 @@
+import tkinter
+
+window = tkinter.Tk()
+window['background']='blue'
+w = 300
+h = 200
+ws = window.winfo_screenwidth()
+hs = window.winfo_screenheight()
+print(ws)
+x = (ws/2) - (w/2)   
+y = (hs/2) - (h/2)
+window.title('第一个tkinter应用')
+window.geometry('%dx%d+%d+%d' % (w, h, x, y)) 
+
+label = tkinter.Label(window, text='Hello World!')
+label.pack()
+tkinter.mainloop()
